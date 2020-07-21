320564c7a3450497ee17b35a763cdc58540dea41
Bianca von Poser <biancavonposer@gmail.com>
2020-07-21 12:23:16 -0300

    1stcommit

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV["cloudinary_cloud_name"]
-  config.api_key = ENV["cloudinary_api_key"]
-  config.api_secret = ENV["cloudinary_api_secret"]
+  config.cloud_name = ENV["cloudinary_cloud_name"] || "biancavonposer"
+  config.api_key = ENV["cloudinary_api_key"] || "932417639168141"
+  config.api_secret = ENV["cloudinary_api_secret"] || "32PNi7lGsqyuP3iAJBzjzkUvcug"
   config.cdn_subdomain = true
 end
---
+++ db/calculator.rb
@@ -0,0 +1,6 @@
+class Calculator
+	@@number = (1..1000)
+	if @@number / 3 = (1.1000) puts 'buzz'
+	if @@number / 5 = (1..1000) puts 'fizz'
+	if @@number / 3 = (1..1000) and @@number / 5 = (1..1000) puts 'buzzfizz'
+		
